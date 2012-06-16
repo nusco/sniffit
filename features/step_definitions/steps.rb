3facad284afe639d695388ad30f43e3b51a18813
Paolo "Nusco" Perrotta <paolo.nusco.perrotta@gmail.com>
2012-06-16 15:22:42 +0200

    first (bugged) implementation

---
+++ features/step_definitions/steps.rb
@@ -0,0 +1,62 @@
+require 'rest_client'
+require 'rspec'
+
+#SERVER = "http://snippit.heroku.com"
+SERVER = "http://localhost:9292"
+
+Before do
+  @device = @tag = nil
+  RestClient.delete "#{SERVER}\/"
+end
+
+When /^I have a device with id "(.*?)" and a tag "(.*?)"$/ do |device, tag|
+  @device, @tag = device, tag
+end
+
+When /^I have a device with id "(.*?)" and a paired tag "(.*?)"$/ do |device, tag|
+  @device, @tag = device, tag
+  @response = RestClient.put "#{SERVER}\/api/#{device}/linked_to/#{tag}", {}
+end
+
+Then /^I HTTP PUT to http:\/\/www\.snipph\.it\/(.*?)$/ do |url|
+  @response = RestClient.put "#{SERVER}\/#{url}", {}
+end
+
+When /^somebody does an HTTP PUT to http:\/\/www\.snipph\.it\/api\/device_at\/(\d+\.\d+)\/(\d+\.\d+)\/found\/(.*?)$/ do |lat, long, tag|
+  @lat, @long = lat, long
+end
+
+When /^I lose the link to the tag$/ do
+  @response = RestClient.put "#{SERVER}\/api/#{@device}/lost_link_to/#{@tag}", {}
+end
+
+When /^I've lost the link to the tag$/ do
+  @response = RestClient.put "#{SERVER}\/api/#{@device}/lost_link_to/#{@tag}", {}
+end
+
+When /^the user confirms that the tag is lost$/ do
+end
+
+When /^I have a device located at (\d+\.\d+), (\d+\.\d+)$/ do |lat, long|
+  @lat, @long = lat, long
+end
+
+When /^I pick up a broadcast from a tag with id "(.*?)"$/ do |id|
+end
+
+Then /^I poll HTTP GET http:\/\/www\.snipph\.it\/(.*?)$/ do |url|
+  begin
+    @response = RestClient.get "#{SERVER}\/#{url}"
+    @return_code = @response.code
+  rescue Exception => e
+    @return_code = 404
+  end
+end
+
+Then /^I get a (\d+)$/ do |return_code|
+  @return_code.to_s.should == return_code
+end
+
+Then /^I see the geospatial data of the lost device in the response$/ do
+  @response.body.should == "#{@lat},#{@long}"
+end
--- lib/sniffit.rb
+++ lib/sniffit.rb
@@ -1,16 +1,32 @@
 require 'bundler/setup'
 require 'sinatra'
 
-class Sniffit < Sinatra::Base
-  put '/api/:device/linked_to/:tag' do
-    @links ||= {}
-    @links[params[:device]] = params[:tag]
+$TAG_LOCATION = {}
+$LINKED_LOCATION = [0, 0]
+
+class Sniffit < Sinatra::Base  
+  delete '/' do
+    $TAG_LOCATION = {}
+  end
+
+  put '/api/:device/linked_to/:tag' do |device, tag|
+    $TAG_LOCATION[tag] = $LINKED_LOCATION
     200
   end
 
-  put '/api/:device/lost_link_to/:tag' do
-    @links ||= {}
-    @links[params[:device]] = nil
+  put '/api/:device/lost_link_to/:tag' do |device, tag|
+    $TAG_LOCATION[tag] = nil
     200
+  end
+  
+  put '/api/device_at/:lat/:long/found/:tag' do |lat, long, tag|
+    $TAG_LOCATION[tag] = [lat, long]
+    200
+  end
+  
+  get '/api/lost_tags/:tag' do |tag|
+    location = $TAG_LOCATION[tag]
+    return 404 unless location
+    [200, {}, location.join(',')]
   end
 end
